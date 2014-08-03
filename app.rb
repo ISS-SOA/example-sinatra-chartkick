6a54eccf60a039e3435ed0fdac765d2bb6850230
Soumya Ray <soumya.ray@gmail.com>
2014-08-03 18:52:40 +0800

    Change example to use local variables from controller in view
    - change example to Taiwan ethnic group stats
    - app.rb: setup instance vars in controller
    - index.erb: use @title, @data, @source

--- app.rb
+++ app.rb
@@ -2,7 +2,12 @@ require 'sinatra/base'
 require 'chartkick'
 
 class ChartExample < Sinatra::Base
+  attr_accessor :title, :data, :source
+
   get '/' do
+    @title = 'Taiwan Ethnic Composition'
+    @data = {'Hoklo' => 70, 'Hakka' => 15, 'mainlander' => 13, 'indigenous' => 2}
+    @source = 'http://en.wikipedia.org/wiki/Demographics_of_Taiwan'
     erb :index
   end
 end
